2925240d85bcc9c976ecea2c8ac5b8fc6d62d505
Rafael Gonzalez <astrorafael@yahoo.es>
2024-11-26 15:03:59 +0100

    plot_overlapped working

--- src/licaplot/csv.py
+++ src/licaplot/csv.py
@@ -42,7 +42,7 @@ from . import MONOCROMATOR_FILTERS_LABELS
 # -----------------------
 
 log = logging.getLogger(__name__)
-
+MARKERS = ['+','.','o','-']
 
 # -----------------
 # Matplotlib styles
@@ -55,46 +55,21 @@ plt.style.use("licaplot.resources.global")
 # Auxiliary functions
 # -------------------
 
-
-def mpl_plot_single(
-    table: Table,
-    title: str,
-    label: str,
-    filters: Optional[bool],
-) -> None:
-    fig, axes = plt.subplots(nrows=1, ncols=1)
-    # fig.suptitle("Corrected Spectral Response plot")
-    axes.set_xlabel(table.columns[0].name)
-    axes.set_ylabel(table.columns[1].name)
-    axes.plot(table.columns[0], table.columns[1], marker="+", linewidth=1)
-    axes.set_title(label)
-    if filters:
-        for filt in MONOCROMATOR_FILTERS_LABELS:
-            axes.axvline(filt["wavelength"], linestyle=filt["style"], label=filt["label"])
-    axes.grid(True, which="major", color="silver", linestyle="solid")
-    axes.grid(True, which="minor", color="silver", linestyle=(0, (1, 10)))
-    axes.minorticks_on()
-    axes.legend()
-    plt.show()
-
-
 def mpl_plot_overlapped(
     tables: Table,
     title: str,
     labels: Iterable[str],
     filters: Optional[bool],
 ) -> None:
+    """Plot up to 4 datasets in the same axes"""
     fig, axes = plt.subplots(nrows=1, ncols=1)
     fig.suptitle("Overlapped Plot")
     axes.set_title(title)
     axes.set_xlabel(tables[0].columns[0].name)
     axes.set_ylabel(tables[0].columns[1].name)
-    for i, table in enumerate(tables):
-        if labels:
-            axes.plot(table.columns[0], table.columns[1], marker="+", linewidth=1)
-        else:
-            log.info(labels[i])
-            axes.plot(table.columns[0], table.columns[1], marker="+", linewidth=1, label=labels[i])
+    N = len(tables)
+    for table, label, marker in zip(tables, labels, MARKERS[:N]):
+        axes.plot(table.columns[0], table.columns[1], marker=marker, linewidth=1, label=label)
     if filters:
         for filt in MONOCROMATOR_FILTERS_LABELS:
             axes.axvline(filt["wavelength"], linestyle=filt["style"], label=filt["label"])
@@ -130,10 +105,10 @@ def validate_inputs(*args):
 def csvs(args: Namespace) -> None:
     validate_inputs(args.input_files, args.labels)
     tables = [astropy.io.ascii.read(f) for f in args.input_files]
-    if len(tables) == 1:
-        mpl_plot_single(table=tables[0], title="Hello", label=args.labels[0], filters=args.filters)
-    elif len(tables) > 1 and args.overlap:
-        mpl_plot_overlapped(tables=tables, title="Hello", labels=args.labels, filters=args.filters)
+    if args.overlap:
+        mpl_plot_overlapped(tables=tables, title="Generic CSV Plot", labels=args.labels, filters=args.filters)
+    else:
+        pass
 
 
 # ===================================
