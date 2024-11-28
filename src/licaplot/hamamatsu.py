6e1bac59ef82769186f1916f25517d81b5fc0202
astrorafael <astrorafael@gmail.com>
2024-11-29 00:49:43 +0100

    Added 2 interpolation methods

--- src/licaplot/hamamatsu.py
+++ src/licaplot/hamamatsu.py
@@ -12,9 +12,11 @@
 
 import os
 import logging
+from enum import Enum
 
 # Typing hints
 from argparse import ArgumentParser, Namespace
+from typing import Tuple
 
 # ---------------------
 # Thrid-party libraries
@@ -25,8 +27,9 @@ import matplotlib.pyplot as plt
 import astropy.io.ascii
 import astropy.units as u
 from astropy.constants import astropyconst20 as const
-from astropy.table import Table
+from astropy.table import Table, QTable
 
+import scipy.interpolate
 from lica.cli import execute
 from lica.validators import vfile
 
@@ -37,32 +40,12 @@ from lica.validators import vfile
 
 from ._version import __version__
 from .utils.mpl import plot_overlapped
+
 # -----------------------
 # Module global variables
 # -----------------------
 
 log = logging.getLogger(__name__)
-
-METADATA = {
-    "Global": {
-        "Manufacturer": "Hamamatsu",
-        "Model": "S2281-04",
-        "Serial": 1097,
-        "Photosensitive area": 50 * (u.mm**2),
-        "Dark current": 50 * (u.pA),
-    },
-    # stage1 processing
-    "History": [
-        {
-            "Label": "NPL",
-            "Description": "NPL Calibration",
-            "Date": None,
-            "Start Wavelength": 350 * u.nm,
-            "End Wavelength": 1000 * u.nm,
-            "Additional Processing": None,
-        },
-    ],
-}
 
 # -----------------
 # Matplotlib styles
@@ -72,9 +55,122 @@ METADATA = {
 plt.style.use("licaplot.resources.global")
 
 
+class COL(Enum):
+    """Calibration Table Columns"""
+    WAVE = "Wavelength"
+    RESP = "Responsivity"
+    QE = "QE"
+
+
 # -------------------
 # Auxiliary functions
 # -------------------
+
+
+def quantum_efficiency(wavelength: np.ndarray, responsivity: np.ndarray) -> np.ndarray:
+    """Computes the Quantum Efficiency given the Responsivity in A/W"""
+    K = (const.h * const.c) / const.e
+    return np.round(K * responsivity / wavelength.to(u.m), decimals=5) * u.dimensionless_unscaled
+
+
+def save_table(path: str, table: Table) -> None:
+    output_path, _ = os.path.splitext(path)
+    output_path += ".ecsv"
+    log.info("Generating %s", output_path)
+    table.write(output_path, format="ecsv", overwrite=True)
+
+
+def create_npl_table(npl_path: str) -> Table:
+    log.info("Converting NPL Calibration CSV to Astropy Table: %s", npl_path)
+    table = astropy.io.ascii.read(
+        npl_path,
+        delimiter=";",
+        data_start=1,
+        names=(COL.WAVE.value, COL.RESP.value),
+        converters={COL.WAVE.value: np.float64, COL.RESP.value: np.float64},
+    )
+    table[COL.WAVE.value] = np.round(table[COL.WAVE.value], decimals=0) * u.nm
+    table[COL.RESP.value] = table[COL.RESP.value] * (u.A / u.W)
+    table[COL.QE.value] = quantum_efficiency(table[COL.WAVE.value], table[COL.RESP.value])
+    table.meta = {
+        "Global": {
+            "Manufacturer": "Hamamatsu",
+            "Model": "S2281-04",
+            "Serial": 1097,
+            "Photosensitive area": 50 * (u.mm**2),
+            "Dark current": 50 * (u.pA),
+        },
+        # stage1 processing
+        "History": [
+            {
+                "Description": "Created NPL Calibration Table",
+                "Date": None,
+                "Start Wavelength": np.min(table[COL.WAVE.value]) * u.nm,
+                "End Wavelength": np.max(table[COL.WAVE.value]) * u.nm,
+            },
+        ],
+    }
+    log.info("Generated table is\n%s", table.info)
+    return table
+
+
+def create_datasheet_table(path: str, x: float, y: float, threshold: float) -> Tuple[Table, Table]:
+    """Returns the original NPL Table, the Datasheet"""
+    log.info("Converting Datasheet CSV to Astropy Table: %s", path)
+    table = astropy.io.ascii.read(
+        path,
+        delimiter=";",
+        data_start=1,
+        names=(COL.WAVE.value, COL.RESP.value),
+        converters={COL.WAVE.value: np.float64, COL.RESP.value: np.float64},
+    )
+    table[COL.WAVE.value] = (table[COL.WAVE.value] + x) * u.nm
+    table[COL.RESP.value] = np.round(table[COL.RESP.value] + y, decimals=5) * (u.A / u.W)
+    table[COL.QE.value] = quantum_efficiency(table[COL.WAVE.value], table[COL.RESP.value])
+    log.info("Selecting new datapoints outside the initial NPL data")
+    mask = table[COL.WAVE.value] >= (threshold + 1.0)
+    return table, table[mask]
+
+
+def combine_tables(table1: Table, table2: Table, x: float, y: float) -> Table:
+    log.info("Combining tables")
+    table = astropy.table.vstack([table1, table2])
+    history = {
+        "Description": "Datasheet + PlotDigitizer extraction",
+        "Date": "2024-01-01",
+        "Start Wavelength": np.min(table2[COL.WAVE.value]) * u.nm,
+        "End Wavelength": np.max(table2[COL.WAVE.value]) * u.nm,
+        "Additional Processing": {
+            "Comment": "Added offset to match input datasheet curve to NPL Calibration curve",
+            "X Offset": x * u.nm,
+            "Y Offset": y * (u.A / u.W),
+        },
+    }
+    table.meta["History"].append(history)
+    return table
+
+
+def interpolate_table(table: Table, method: str, resolution: int) -> Table:
+    wavelength = np.arange(350.0, 1051.0, resolution) * u.nm
+    if method == "linear":
+        responsivity = np.round(
+            np.interp(wavelength, table[COL.WAVE.value], table[COL.RESP.value]), decimals=5
+        ) * (u.A / u.W)
+    else:
+        interpolator = scipy.interpolate.Akima1DInterpolator(table[COL.WAVE.value], table[COL.RESP.value])
+        responsivity = np.round(interpolator(wavelength), decimals=5) * (u.A / u.W)
+    qe = quantum_efficiency(wavelength, responsivity)
+    qtable = QTable([wavelength, responsivity, qe], names=(COL.WAVE.value, COL.RESP.value, COL.QE.value))
+    qtable.meta = table.meta
+    history = {
+        "Description": "Resampled calibration data at regular intervals",
+        "Resolution": resolution * u.nm,
+        "Method": "linear" if method == "linear interpolation" else "Akima piecewise cubic polynomials",
+        "Start Wavelength": np.min(qtable[COL.WAVE.value]) * u.nm,
+        "End Wavelength": np.max(qtable[COL.WAVE.value]) * u.nm,
+    }
+    qtable.meta["History"].append(history)
+    return qtable
 
 
 # -----------------------
@@ -82,36 +178,17 @@ plt.style.use("licaplot.resources.global")
 # -----------------------
 
 
-def quantum_efficiency(table: Table) -> np.ndarray:
-    """Computes the Quantum Efficiency given the Responsivity in A/W"""
-    K = (const.h * const.c) / const.e
-    return (K * table["Responsivity"] / table["Wavelength"].to(u.m)).to(u.dimensionless_unscaled)
-
-
 def stage1(args: Namespace) -> None:
-    path = args.input_file
-    log.info("Converting CSV to Astropy Table")
-    table = astropy.io.ascii.read(
-        path,
-        delimiter=";",
-        data_start=1,
-        names=("Wavelength", "Responsivity"),
-        converters={"Wavelength": np.float64, "Responsivity": np.float64},
-    )
-    table["Wavelength"] = np.round(table["Wavelength"], 0) * u.nm
-    table["Responsivity"] = table["Responsivity"] * (u.A / u.W)
-    table["QE"] = quantum_efficiency(table)
-    table.meta = METADATA
-    log.info("Generated table is\n%s", table.info)
-    output_path, _ = os.path.splitext(path)
+    table = create_npl_table(npl_path=args.input_file)
+    output_path, _ = os.path.splitext(args.input_file)
     output_path += ".ecsv"
     log.info("Generating %s", output_path)
-    table.write(output_path, overwrite=True)
+    table.write(output_path, delimiter=",", overwrite=True)
     if args.plot:
         plot_overlapped(
-            title=args.title,
+            title=f"{args.title} #{table.meta['Global']['Serial']}",
             tables=[table],
-            labels=[METADATA["History"][0]["Label"]],
+            labels=["NPL"],
             filters=False,
             x=0,
             y=1,
@@ -123,21 +200,15 @@ def stage2(args: Namespace) -> None:
     """Iterative merge curves and saves the combined results"""
     log.info("Loading NPL ECSV calibration File: %s", args.npl_file)
     npl_table = astropy.io.ascii.read(args.npl_file, format="ecsv")
-    log.info("Loading datasheet CSV calibration File: %s", args.input_file)
-    datasheet_table = astropy.io.ascii.read(
-        args.input_file,
-        delimiter=";",
-        data_start=1,
-        names=("Wavelength", "Responsivity"),
-        converters={"Wavelength": np.float64, "Responsivity": np.float64},
+    threshold = np.max(npl_table[COL.WAVE.value]) + 1
+    datasheet_table, sliced_table = create_datasheet_table(
+        path=args.input_file,
+        x=args.x,
+        y=args.y,
+        threshold=threshold,
     )
-    datasheet_table["Wavelength"] = (datasheet_table["Wavelength"] + args.x) * u.nm
-    datasheet_table["Responsivity"] = np.round(datasheet_table["Responsivity"] + args.y, 5) * (
-        u.A / u.W
-    )
-    datasheet_table["QE"] = quantum_efficiency(datasheet_table)
     plot_overlapped(
-        title=args.title + " overlapped curves",
+        title=f"{args.title} #{npl_table.meta['Global']['Serial']} overlapped curves",
         tables=[npl_table, datasheet_table],
         labels=["NPL", "datasheet"],
         filters=False,
@@ -145,12 +216,9 @@ def stage2(args: Namespace) -> None:
         y=1,
         linewidth=0,
     )
-    log.info("Selecting new datapoints outside the initial NPL data")
-    mask = datasheet_table["Wavelength"] >= (np.max(npl_table["Wavelength"]) + 1)
-    additional_table = datasheet_table[mask]
     plot_overlapped(
-        title=args.title + " trimmed curves",
-        tables=[npl_table, additional_table],
+        title=f"{args.title} #{npl_table.meta['Global']['Serial']} combined curves",
+        tables=[npl_table, sliced_table],
         labels=["NPL", "datasheet"],
         filters=False,
         x=0,
@@ -158,29 +226,34 @@ def stage2(args: Namespace) -> None:
         linewidth=0,
     )
     if args.save:
-        history = {
-            "Label": "datasheet",
-            "Description": "Datasheet + PlotDigitizer extraction",
-            "Date": "2024-01-01",
-            "Start Wavelength": np.min(npl_table["Wavelength"]),
-            "End Wavelength": np.max(npl_table["Wavelength"]),
-            "Additional Processing": {
-                "Comment": "Added offset to match input datasheet curve to NPL Calibration curve",
-                "X Offset": args.x * u.nm,
-                "Y Offset": args.y * (u.A / u.W),
-            },
-        }
-        merged_table = astropy.table.vstack([npl_table, additional_table])
-        merged_table.meta["History"].append(history)
+        merged_table = combine_tables(npl_table, sliced_table, args.x, args.y)
         output_path, _ = os.path.splitext(args.npl_file)
         output_path += "-Combined.ecsv"
         log.info("Generating %s", output_path)
-        merged_table.write(output_path, overwrite=True)
+        merged_table.write(output_path, delimiter=",", overwrite=True)
+
 
 def stage3(args: Namespace) -> None:
-    log.info("Loading NPL + Datasheet ECSV calibration File: %s", args.npl_file)
-    merged_table = astropy.io.ascii.read(args.npl_file, format="ecsv")
-    log.info(merged_table.info)
+    log.info("Loading NPL + Datasheet ECSV calibration File: %s", args.input_file)
+    table = astropy.io.ascii.read(args.input_file, format="ecsv")
+    log.info(table.info)
+    interpolated_table = interpolate_table(table, args.method, args.resolution)
+    log.info(interpolated_table.info)
+    output_path, _ = os.path.splitext(args.input_file)
+    output_path += "-Interpolated.ecsv"
+    log.info("Generating %s", output_path)
+    interpolated_table.write(output_path, delimiter=",", overwrite=True)
+    if args.plot:
+        plot_overlapped(
+            title=f"{args.title} #{table.meta['Global']['Serial']} interpolated curves @ {args.resolution} nm",
+            tables=[table, interpolated_table],
+            labels=["NPL+atasheet", "interpolated"],
+            filters=False,
+            x=0,
+            y=1,
+            linewidth=0,
+        )
+
 
 # ===================================
 # MAIN ENTRY POINT SPECIFIC ARGUMENTS
@@ -188,7 +261,6 @@ def stage3(args: Namespace) -> None:
 
 
 def add_args(parser: ArgumentParser) -> None:
-
     subparser = parser.add_subparsers(dest="command")
     parser_stage1 = subparser.add_parser(
         "stage1", help="Load NPL calibration CSV and convert to ECSV"
@@ -199,14 +271,14 @@ def add_args(parser: ArgumentParser) -> None:
     parser_stage3 = subparser.add_parser(
         "stage3", help="Resamples calibration data to uniform 1nm wavelength step"
     )
-
+    # ------------------------------------------------------------------------
     parser_stage1.add_argument(
         "-i",
         "--input-file",
         type=vfile,
         required=True,
         metavar="<CSV>",
-        help="input CSV with NPL calibration",
+        help="CSV with NPL calibration",
     )
     parser_stage1.add_argument(
         "-p",
@@ -221,7 +293,7 @@ def add_args(parser: ArgumentParser) -> None:
         default="Hamamatsu S2281-04",
         help="Plot title",
     )
-
+    # ------------------------------------------------------------------------
     parser_stage2.add_argument(
         "-n",
         "--npl-file",
@@ -236,12 +308,12 @@ def add_args(parser: ArgumentParser) -> None:
         type=vfile,
         required=True,
         metavar="<CSV>",
-        help="aditions CSV with datasheet calibration values",
+        help="CSV with datasheet calibration values",
     )
     parser_stage2.add_argument(
         "-x",
         "--x",
-        type=np.float64,
+        type=float,
         default=0.0,
         metavar="<X offset>",
         help="X (wavelength) offset to apply to input CSV file (defaults to %(default)f)",
@@ -249,7 +321,7 @@ def add_args(parser: ArgumentParser) -> None:
     parser_stage2.add_argument(
         "-y",
         "--y",
-        type=np.float64,
+        type=float,
         default=0.0,
         metavar="<Y offset>",
         help="Y (responsivity) offset to apply to input CSV file (defaults to %(default)f)",
@@ -267,20 +339,50 @@ def add_args(parser: ArgumentParser) -> None:
         default="Hamamatsu S2281-04",
         help="Plot title",
     )
-
+    # ------------------------------------------------------------------------
     parser_stage3.add_argument(
-        "-n",
-        "--npl-file",
+        "-i",
+        "--input-file",
         type=vfile,
         required=True,
         metavar="<NPL ECSV>",
         help="ECSV with NPL + Datasheet calibration",
     )
+    parser_stage3.add_argument(
+        "-m",
+        "--method",
+        type=str,
+        choices=("linear", "cubic"),
+        default="linear",
+        help="Interpolation method (defaults to %(default)d nm)",
+    )
+    parser_stage3.add_argument(
+        "-r",
+        "--resolution",
+        type=int,
+        choices=tuple(range(1, 11)),
+        default=1,
+        metavar="<N nm>",
+        help="Interpolate at equal resolution (defaults to %(default)d nm)",
+    )
+    parser_stage3.add_argument(
+        "-t",
+        "--title",
+        type=str,
+        default="Hamamatsu S2281-04",
+        help="Plot title",
+    )
+    parser_stage3.add_argument(
+        "-p",
+        "--plot",
+        action="store_true",
+        help="Plot file too",
+    )
+
 
 # ================
 # MAIN ENTRY POINT
 # ================
-
 
 COMMAND_TABLE = {
     "stage1": stage1,
