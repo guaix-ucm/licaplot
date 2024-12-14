fixed jupyter notebook and QE units computation

src/licaplot/utils/processing.py
@@ -401,14 +401,9 @@ def active_process(
             sensor_qe = (
                 photod_qe
                 * (sensor_area / photod_area)
-                * (sensor_table[sensor_column] / photod_table[TBCOL.CURRENT])
-            )
-            unit = (
-                (sensor_table[sensor_column].unit / photod_table[TBCOL.CURRENT].unit)
-                * photod_qe.unit
-                * gain
-            )
-            sensor_table[PROCOL.SPECTRAL] = np.round(sensor_qe, decimals=5) * unit.decompose()
+                * (sensor_table[sensor_column] * gain) / photod_table[TBCOL.CURRENT]
+            ).decompose()
+            sensor_table[COL.QE] = np.round(sensor_qe, decimals=5) * u.dimensionless_unscaled
             sensor_table.meta["Processing"]["using photodiode"] = model
             sensor_table.meta["Processing"]["processed"] = True
             sensor_table.meta["History"].append(
@@ -442,7 +437,7 @@ def passive_process(photodiode_dict: DiodeDict, filter_dict: DeviceDict) -> Devi
                     f"Trimmed to [{wave_low:04d}-{wave_high:04d}] nm wavelength range"
                 )
                 filter_dict[key][i] = filter_table  # Necessary to capture the new table in the dict
-            transmission = filter_table[TBCOL.CURRENT] / photod_table[TBCOL.CURRENT]
+            transmission = (filter_table[TBCOL.CURRENT] / photod_table[TBCOL.CURRENT]).decompose()
             filter_table[PROCOL.PHOTOD_CURRENT] = photod_table[TBCOL.CURRENT]
             filter_table[PROCOL.TRANS] = (
                 np.round(transmission, decimals=5) * u.dimensionless_unscaled