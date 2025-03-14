8b0f61e0708c64706640cad4aea24d40dd5fc80d
astrorafael <astrorafael@gmail.com>
2025-03-14 16:46:58 +0100

    lica-file utility merged into lica-meta

--- src/licatools/metadata.py
+++ src/licatools/metadata.py
@@ -12,6 +12,7 @@
 
 import os
 import logging
+from datetime import datetime
 from argparse import ArgumentParser, Namespace
 
 
@@ -20,6 +21,7 @@ from argparse import ArgumentParser, Namespace
 # ---------------------
 
 from lica.cli import execute
+from lica.validators import vfile
 
 # ------------------------
 # Own modules and packages
@@ -45,10 +47,40 @@ log = logging.getLogger(__name__)
 # Auxiliary functions
 # -------------------
 
+def vdate(datestr: str|list[str]) -> datetime:
+    """Date & time validator for the command line interface"""
+    if datestr.lower() == "now":
+        date = datetime.now()  # Naive date, current Local date no tzinfo
+    else:
+        # These are all naive dates after all, the user may be specifingu UTC or Local Time
+        # Except for the last one ...
+        date = None
+        formats = ("%Y-%m", "%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%SZ")
+        for fmt in formats:
+            try:
+                date = datetime.strptime(datestr, fmt)
+            except ValueError:
+                pass
+        if date is None:
+            raise ValueError(f"{datestr} is not a valid date format ({formats})")
+    return date
 
 # -----------------------
 # AUXILIARY MAIN FUNCTION
 # -----------------------
+
+def cli_file_time(args: Namespace):
+    log.info(args)
+    if args.from_date:
+        modif_time =  args.from_date.timestamp()
+        access_time = modif_time
+    else:
+        stats = os.stat(args.from_file)
+        modif_time = stats.st_mtime
+        access_time = stats.st_atime
+    log.info("Setting '%s' modification time to %s", args.to_file, datetime.fromtimestamp(modif_time))
+    timestamps = (access_time, modif_time)
+    os.utime(args.to_file, timestamps)
 
 
 def cli_generate(args: Namespace) -> None:
@@ -63,6 +95,36 @@ def cli_generate(args: Namespace) -> None:
 # MAIN ENTRY POINT SPECIFIC ARGUMENTS
 # ===================================
 
+def from_source() -> ArgumentParser:
+    """Common Options for subparsers"""
+    parser = ArgumentParser(add_help=False)
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument(
+        "-ff",
+        "--from-file",
+        type=vfile,
+        help="File to copy modification time  (defaults to %(default)s)",
+    )
+    group.add_argument(
+        "-fd",
+        "--from-date",
+        type=vdate,
+        help="Date modification time (defaults to %(default)s = current date)",
+    )
+    return parser
+
+
+def to_file() -> ArgumentParser:
+    """Common Options for subparsers"""
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-tf",
+        "--to-file",
+        required=True,
+        type=vfile,
+        help="File to overwrite modification time",
+    )
+    return parser
 
 def globpat() -> ArgumentParser:
     parser = ArgumentParser(add_help=False)
@@ -97,6 +159,10 @@ def add_args(parser: ArgumentParser) -> None:
         help="Generates a metadata file for the acquistion files in this directory",
     )
     parser_scan.set_defaults(func=cli_generate)
+    parser_touch = subparser.add_parser(
+        "touch", parents=[to_file(), from_source()], help="Set file modification time"
+    )
+    parser_touch.set_defaults(func=cli_file_time)
 
 
 # ================
